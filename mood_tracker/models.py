84c0a56beb1014da8a01ce4789221f42dd765a7a
alcl2000 <amy.555.lewis@gmail.com>
2023-11-22 16:27:19 +0000

    add: create achievement models

--- mood_tracker/models.py
+++ mood_tracker/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Achievements(models.Model):
+    #creates the model for user achivement tracking
+    achievement_id = models.AutoField(primary_key=True, unique=True)
+    achievement_name = models.CharField(unique=True, max_length=100)
+    achievement_icon = models.ImageField(
+                        upload_to='images/', default='../default-pic_ls0v0g.png')
+    achievement_description = models.CharField(max_length=250)
