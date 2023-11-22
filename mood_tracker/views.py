cf5c9f610518cf0e84a22d397ba7aeb70a85c332
alcl2000 <amy.555.lewis@gmail.com>
2023-11-22 17:24:16 +0000

    add: achievement list

--- mood_tracker/views.py
+++ mood_tracker/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic, View
+from models.py import Achievement
+
 
 # Create your views here.
+class AchievementList(generic.ListView):
+    model = Achievement
+    queryset = Category.objects.order_by('-category_id')
+
