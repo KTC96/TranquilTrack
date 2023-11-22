c8df7853299494dd7ad5f250de096f6f40efd4d4
alcl2000 <amy.555.lewis@gmail.com>
2023-11-22 22:10:02 +0000

    add: view

--- mood_tracker/views.py
+++ mood_tracker/views.py
@@ -7,4 +7,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class Home(LoginRequiredMixin, TemplateView):
     template_name = 'index.html'
 
-   
+
+class AchievementList(generic.ListView):
+    model = Achievement
+    queryset = Category.objects.order_by('-category_id')
+    template_name = 'achievements.html'
+
+
