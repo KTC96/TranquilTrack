1bea0d31b86d5b0040a9c68bcb9fe04cc5dc3917
alcl2000 <amy.555.lewis@gmail.com>
2023-11-22 22:09:51 +0000

    add: admin model

--- mood_tracker/admin.py
+++ mood_tracker/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from views.py import Achievements
 
 # Register your models here.
+@admin.register(Achievements):
+class AchievementsAdmin(SummernoteModelAdmin):
+    list_display = ('achievement_id', 'achievement_name', 'achievement_description')
