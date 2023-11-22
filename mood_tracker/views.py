20c906ca32501106392df3d60a1a958b2cfdf6dd
KTC96 <kyleclow96@gmail.com>
2023-11-22 15:01:15 +0000

    add templates, render basic view of index, style and add to base.html

--- mood_tracker/views.py
+++ mood_tracker/views.py
@@ -8,3 +8,8 @@ class AchievementList(generic.ListView):
     model = Achievement
     queryset = Category.objects.order_by('-category_id')
 
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class Home(TemplateView):
+    template_name = 'index.html'
