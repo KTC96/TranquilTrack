e0fb6d9badbacac70f7b20629212178c82ec356a
douglas <douglasmaxton@gmail.com>
2023-11-23 17:50:37 +0000

    created title on each axis

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,5 +1,7 @@
+// gets a canvas element from index.html file
 const ctx = document.getElementById("lineChart");
 
+// creates charts on a canvas element
 new Chart(ctx, {
   type: "line",
   data: {
@@ -8,28 +10,36 @@ new Chart(ctx, {
         data: [20, 50, 100, 75, 25, 0],
         label: "Left dataset",
 
-        // This binds the dataset to the left y axis
+        // This binds the dataset to the left y-axis
         yAxisID: "left-y-axis",
       },
       {
         data: [0.1, 0.5, 1.0, 2.0, 1.5, 0],
         label: "Right dataset",
 
-        // This binds the dataset to the right y axis
+        // This binds the dataset to the right y-axis
         yAxisID: "right-y-axis",
       },
     ],
     labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    xAxisID: "bottom-x-axis",
   },
+  // options to set with chart
   options: {
     scales: {
       "left-y-axis": {
         type: "linear",
         position: "left",
+        title: { display: true, text: "Left Y Axis" },
       },
       "right-y-axis": {
         type: "linear",
         position: "right",
+        title: { display: true, text: "Right Y Axis" },
+      },
+      "bottom-x-axis": {
+        position: "bottom",
+        title: { display: true, text: "Bottom X Axis" },
       },
     },
   },
