2c3c50727d0f92f1c3181a3a2b377431e9a6aad2
douglas <douglasmaxton@gmail.com>
2023-11-23 15:53:08 +0000

    changed the labels on the graph

--- static/js/chart.js
+++ static/js/chart.js
@@ -19,14 +19,14 @@ const data = {
   labels: labels,
   datasets: [
     {
-      label: "My First Dataset",
+      label: "Exercise",
       data: [65, 59, 80, 81, 56, 55, 40],
       fill: false,
       borderColor: "rgb(75, 192, 192)",
       tension: 0.1,
     },
     {
-      label: "My Second Dataset",
+      label: "Sleep Pattern",
       data: [100, 20, 42, 23, 40, 60, 10],
       fill: false,
       borderColor: "rgb(66, 135, 245)",
