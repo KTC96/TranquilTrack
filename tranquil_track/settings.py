944968c865d18bce9d57827f981fb2a690b4e279
Veronika Nemergutova <veronika.nemergutova@gmail.com>
2023-11-24 21:11:59 +0000

    Fix nav ul padding

--- tranquil_track/settings.py
+++ tranquil_track/settings.py
@@ -169,3 +169,6 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+ 
+
+CSRF_TRUSTED_ORIGINS = ['https://8000-veronika282-tranquiltra-a3dpycqk9sb.ws-eu106.gitpod.io']
